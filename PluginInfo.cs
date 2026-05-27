6c9d3adfb7ecae82986dc92adb4c261a1490e2e7
DeezVr <normajperez1030@gmail.com>
2026-05-26 20:06:29 -0400

    Source code!

---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace FortniteEmoteWheel
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "com.goldentrophy.deez.gorillatag.fortniteemotewheel";
+        public const string Name = "FortniteEmoteWheel";
+        public const string Version = "1.3.6";
+    }
+}
