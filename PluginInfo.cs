a606422346bccb7caf99047e4db6f5ea6eed64f8
DeezVr <normajperez1030@gmail.com>
2026-07-20 00:56:00 +0200

    New source

---
+++ PluginInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace FortniteEmoteWheel
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "com.deez.fortniteemotewheel";
+        public const string Name = "FortniteEmoteWheel";
+        public const string Version = "1.3.9";
+        public const string HamburburDataUrl = "https://hamburbur.uk/data";
+        public const string TelemetryWssUrl = "wss://hamburbur.uk/telemetry";
+    }
+}
