1db775229c3186006562d7a7a09c653de8c76650
DeezVr <normajperez1030@gmail.com>
2026-09-18 23:03:19 -0400

    Update Constants.cs

--- Constants.cs
+++ Constants.cs
@@ -3,7 +3,7 @@
     internal class Constants
     {
         public const string PluginGuid = "com.deez.fortniteemotewheel";
-        public const string PluginName = "Deez's FortniteEmoteWheel";
+        public const string PluginName = "FortniteEmoteWheel";
         public const string PluginVersion = "1.5.4";
         public const string Hashkey = "github.com/DeezVrOfficial/FortniteEmoteWheel";
     }
